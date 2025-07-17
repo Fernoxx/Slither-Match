8b1f32196c4ff73a25f44a1a57bc721ce4b1fa36
Fernoxx <thenameisferno@gmail.com>
2025-07-17 18:57:26 +0530

    wrap connector in array

--- lib/wagmiConfig.ts
+++ lib/wagmiConfig.ts
@@ -1,7 +1,8 @@
-import { farcasterMiniApp } from "@farcaster/miniapp-wagmi-connector"
+import { farcasterMiniApp, farcasterChains } from "@farcaster/miniapp-wagmi-connector"
 import { createConfig } from "wagmi"
 
 export const config = createConfig({
+  chains: farcasterChains,
   connectors: [() => farcasterMiniApp()],
   ssr: true,
 })
