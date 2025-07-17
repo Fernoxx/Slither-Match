55607c03674bfc2343fa9effa587b5c7cedc9505
Fernoxx <thenameisferno@gmail.com>
2025-07-17 18:48:22 +0530

    remove farcasterChains and fix connector type

--- lib/wagmiConfig.ts
+++ lib/wagmiConfig.ts
@@ -1,6 +1,7 @@
-import { farcasterMiniApp } from "@farcaster/miniapp-wagmi-connector";
-import { createConfig } from "wagmi";
+import { createConfig } from "wagmi"
+import { farcasterMiniApp } from "@farcaster/miniapp-wagmi-connector"
 
-export const config = createConfig(
-  farcasterMiniApp({ chains: farcasterChains })
-);
+export const config = createConfig({
+  connectors: farcasterMiniApp(),
+  ssr: true,
+})
