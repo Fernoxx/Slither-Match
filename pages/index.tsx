ae62ebef3e76646afd05064fa1d99ceb70716b3b
Ferno <154271878+Fernoxx@users.noreply.github.com>
2025-07-27 02:43:22 +0530

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -21,7 +21,6 @@ export default function Home() {
   const [gameScore, setGameScore] = useState(0)
   const [gameEnded, setGameEnded] = useState(false)
   const [isPaidLobby, setIsPaidLobby] = useState(false)
-  const [isWinner, setIsWinner] = useState(false)
 
   // Connect Farcaster wallet (only for paid lobby)
   const connectWallet = useCallback(async () => {
