0c35d121fbd92079464edaacd9640bce09afeeac
Ferno <154271878+Fernoxx@users.noreply.github.com>
2025-07-28 01:05:01 +0530

    Update wagmiConfig.ts

--- lib/wagmiConfig.ts
+++ lib/wagmiConfig.ts
@@ -1,3 +1,16 @@
 import { http, createConfig } from 'wagmi'
 import { base, baseSepolia } from 'wagmi/chains'
 import { farcasterMiniApp } from '@farcaster/miniapp-wagmi-connector'
+
+// Reliable Base RPC endpoints
+const BASE_RPC_URL = 'https://base.blockpi.network/v1/rpc/public'
+const BASE_SEPOLIA_RPC_URL = 'https://base-sepolia.blockpi.network/v1/rpc/public'
+
+export const config = createConfig({
+  chains: [base, baseSepolia],
+  connectors: [farcasterMiniApp()],
+  ssr: true,
+  transports: {
+    [baseSepolia.id]: http(BASE_SEPOLIA_RPC_URL)
+  }
+})
