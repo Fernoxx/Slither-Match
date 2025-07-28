b207e13ee28a07a0b4e19b33b1e0a998269fd2f4
Ferno <154271878+Fernoxx@users.noreply.github.com>
2025-07-29 04:18:12 +0530

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -189,11 +189,7 @@ export default function Home() {
               onScoreChange={() => {}}
               onGameOver={() => {}}
               onGameWin={() => {}}
-            />
-          </div>
-        </div>
-      )}
-
+    
                            {currentView === 'paid-lobby' && !gameStarted && !gameEnded && (
          <div className="text-center z-10">
            <div className="bg-[#1a1a2e] border border-[#2d2d5e] rounded-lg p-8 mb-6 min-w-[400px]">
